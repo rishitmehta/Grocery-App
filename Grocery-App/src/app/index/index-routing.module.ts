4742eae8975407b610d945cd80a4f788a9d724b8
Himanshu Gupta <hkghimanshu129867@gmail.com>
2020-05-19 16:40:54 +0530

    index routing changes

--- Grocery-App/src/app/index/index-routing.module.ts
+++ Grocery-App/src/app/index/index-routing.module.ts
@@ -6,7 +6,24 @@ import { IndexPage } from './index.page';
 const routes: Routes = [
   {
     path: '',
-    component: IndexPage  
+    component: IndexPage,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('src/app/pages/welcome/welcome.module')
+        .then(m => m.WelcomePageModule)
+      },
+      {
+        path: 'login',
+        loadChildren: () => import('src/app/pages/login/login.module')
+        .then(m => m.LoginPageModule)
+      },
+      {
+        path: 'signup',
+        loadChildren: () => import('src/app/pages/signup/signup.module')
+        .then(m => m.SignupPageModule)
+      }
+    ]
   }
 ];
 
